9eebe02396068ae0a8cf5e925e5a83b5d18f267f
Shuaib O <shuaib.olurode@decagon.dev>
2022-01-27 15:29:58 +0100

    create main.go file

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	
+	"log"
+	"github.com/OShuaib/Human-resource-management/handler"
+	"github.com/OShuaib/Human-resource-management/database"
+	"github.com/gofiber/fiber/v2"
+	
+)
+
+
+
+
+
+
+
+func main(){
+if err:= database.Connect(); err != nil {
+	log.Fatal(err)
+}
+ 
+	app := fiber.New()
+
+	app.Get("/employee", handler.GetEmployee())
+
+
+	app.Post("/employee", handler.CreateEmployee())
+
+
+	app.Put("/employee/:id",handler.UpdateEmployee())
+
+
+	app.Delete("/employee/:id", handler.DeleteEmployee())
+
+	app.Listen(":300")
+}
